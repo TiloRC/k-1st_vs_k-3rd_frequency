9d054b7aeb1bca452ae89494418b8c8bcd1a6d6d
Tilo Reneau-Cardoso <67246777+TiloRC@users.noreply.github.com>
2022-09-23 21:58:17 -0700

    Update viz.py
    Cleaned up code and added comments

--- viz.py
+++ viz.py
@@ -2,16 +2,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 
-def is_word(word):
-	# English words do not have non-alphanumeric characters
-	return word.isalpha()
 
-words = []
-with open("./top_2000000.txt", 'r') as f:
-    for line in f:
-        values = line.split()
-        if is_word(values[0]):
-        	words.append(values[0])
+def read_most_common_words(filepath):
+        """reads words from  from each line, excluding words with non-letter characters"""
+        words = []
+        with open(filepath, 'r') as f:
+                for line in f:
+                        values = line.split()
+                        if values[0].isalpha():
+        	                words.append(values[0])
+        return words
 
 
 def is_k1(word):
@@ -27,9 +27,9 @@ def is_k3(word):
 	return False
 
 
-
+filepath = "./top_2000000.txt"
+words = read_most_common_words(filepath)
 df = pd.DataFrame()
-
 
 
 df["words"] = words
@@ -40,89 +40,27 @@ df["cum_k1_count"] = df["is_k1_word"].cumsum()
 df["cum_k3_count"] = df["is_k3_word"].cumsum()
 df["k1_freq"] = df["cum_k1_count"]/df["word_rank"]
 df["k3_freq"] = df["cum_k3_count"]/df["word_rank"]
+unaltered_df = df
+num_rows_values = [1000000, 100000, 10000, 1000]
 
-df2 = df
+for num_rows in num_rows_values:
+        df = df.head(num_rows)
+        print("{:,}".format(num_rows), ": ")
+        print("k-1st words: ",df["is_k1_word"].sum() )
+        print("k-3rd words: ",df["is_k3_word"].sum() )
+        plt.figure()
+        plt.title("Top " + "{:,}".format(num_rows) + " words")
+        plt.plot(df["k1_freq"],"r", label = "k-1st")
+        plt.plot(df["k3_freq"],"b", label = "k-3rd")
+        plt.xlabel("Number of words")
+        plt.ylabel("Frequency")
+        plt.legend(loc='best')
+        plt.show()
 
-
-
-
-df = df.head(1000000)
-print("1,000,000: ")
-print("k-1st words: ",df["is_k1_word"].sum() )
-print("k-3rd words: ",df["is_k3_word"].sum() )
-
-plt.figure()
-plt.title("Top " + "1,000,000" + " words")
-plt.plot(df["k1_freq"],"r", label = "k-1st")
-plt.plot(df["k3_freq"],"b", label = "k-3rd")
-plt.xlabel("Number of words")
-plt.ylabel("Frequency")
-plt.legend(loc='best')
-plt.show()
-
-
-
-
-
-df = df.head(100000)
-print("100,000: ")
-print("k-1st words: ",df["is_k1_word"].sum() )
-print("k-3rd words: ",df["is_k3_word"].sum() )
-
-plt.figure()
-plt.title("Top " + "100,000" + " words")
-plt.plot(df["k1_freq"],"r", label = "k-1st")
-plt.plot(df["k3_freq"],"b", label = "k-3rd")
-plt.xlabel("Number of words")
-plt.ylabel("Frequency")
-plt.legend(loc='best')
-plt.show()
-
-
-
-df = df.head(10000)
-print("10,000: ")
-print("k-1st words: ",df["is_k1_word"].sum() )
-print("k-3rd words: ",df["is_k3_word"].sum() )
-
-plt.figure()
-plt.title("Top " + "10,000" + " words")
-plt.plot(df["k1_freq"],"r", label = "k-1st")
-plt.plot(df["k3_freq"],"b", label = "k-3rd")
-plt.xlabel("Number of words")
-plt.ylabel("Frequency")
-plt.legend(loc='best')
-plt.show()
-
-
-
-df = df.head(1000)
-print("1,000: ")
-print("k-1st words: ",df["is_k1_word"].sum() )
-print("k-3rd words: ",df["is_k3_word"].sum() )
-
-plt.figure()
-plt.title("Top " + "1,000" + " words")
-plt.plot(df["k1_freq"],"r", label = "k-1st")
-plt.plot(df["k3_freq"],"b", label = "k-3rd")
-plt.xlabel("Number of words")
-plt.ylabel("Frequency")
-plt.legend(loc='best')
-plt.show()
-
-
-
-
-# ADJUSTED GRAPHS
-# VVVVVVVVVVVVVVV
-
-df = df2
-
-
-
+        
+df = unaltered_df
 df["word_freq"] = .07/df["word_rank"]
 df["cum_word_freq"] = df["word_freq"].cumsum()
-
 df["is_k1_word_adj"]= df["is_k1_word"]*df["word_freq"]
 df["is_k3_word_adj"]= df["is_k3_word"]*df["word_freq"]
 df["cum_k1_count_adj"] = df["is_k1_word_adj"].cumsum()
@@ -130,119 +68,33 @@ df["cum_k3_count_adj"] = df["is_k3_word_adj"].cumsum()
 df["k1_freq_adj"] = df["cum_k1_count_adj"]/df["word_rank"]
 df["k3_freq_adj"] = df["cum_k3_count_adj"]/df["word_rank"]
 
-df3 = df
 
-
-df = df.head(1000000)
-
-plt.figure()
-plt.title("Top " + "1,000,000" + " words")
-plt.plot(df["k1_freq_adj"],"r", label = "k-1st")
-plt.plot(df["k3_freq_adj"],"b", label = "k-3rd")
-plt.xlabel("Number of words")
-plt.ylabel("Adjusted frequency")
-plt.legend(loc='best')
-plt.show()
-
-
-
-
-
-df = df.head(100000)
-
-plt.figure()
-plt.title("Top " + "100,000" + " words")
-plt.plot(df["k1_freq_adj"],"r", label = "k-1st")
-plt.plot(df["k3_freq_adj"],"b", label = "k-3rd")
-plt.xlabel("Number of words")
-plt.ylabel("Adjusted frequency")
-plt.legend(loc='best')
-plt.show()
+adjusted_df = df
+for num_rows in num_rows_values:
+        df = df.head(num_rows)
+        print("k-1st words: ",df["is_k1_word"].sum() )
+        print("k-3rd words: ",df["is_k3_word"].sum() )
+        plt.figure()
+        plt.title("Top " + "{:,}".format(num_rows) + " words")
+        plt.plot(df["k1_freq_adj"],"r", label = "k-1st")
+        plt.plot(df["k3_freq_adj"],"b", label = "k-3rd")
+        plt.xlabel("Number of words")
+        plt.ylabel("Adjusted frequency")
+        plt.legend(loc='best')
+        plt.show()
 
 
 
-df = df.head(10000)
-
-plt.figure()
-plt.title("Top " + "10,000" + " words")
-plt.plot(df["k1_freq_adj"],"r", label = "k-1st")
-plt.plot(df["k3_freq_adj"],"b", label = "k-3rd")
-plt.xlabel("Number of words")
-plt.ylabel("Ajusted frequency")
-plt.legend(loc='best')
-plt.show()
-
-
-
-df = df.head(1000)
-
-plt.figure()
-plt.title("Top " + "1,000" + " words")
-plt.plot(df["k1_freq_adj"],"r", label = "k-1st")
-plt.plot(df["k3_freq_adj"],"b", label = "k-3rd")
-plt.xlabel("Number of words")
-plt.ylabel("Adjusted frequency")
-plt.legend(loc='best')
-plt.show()
-
-
-
-# ADJUSTED LOG GRAPHS
-# VVVVVVVVVVVVVVVVVVV
-
-df = df3
-
-
-
-
-df = df.head(1000000)
-
-plt.figure()
-plt.title("Top " + "1,000,000" + " words")
-plt.plot(np.log(df["k1_freq_adj"]),"r", label = "k-1st")
-plt.plot(np.log(df["k3_freq_adj"]),"b", label = "k-3rd")
-plt.xlabel("Number of words")
-plt.ylabel("Adjusted log frequency")
-plt.legend(loc='best')
-plt.show()
-
-
-
-
-
-df = df.head(100000)
-
-plt.figure()
-plt.title("Top " + "100,000" + " words")
-plt.plot(np.log(df["k1_freq_adj"]),"r", label = "k-1st")
-plt.plot(np.log(df["k3_freq_adj"]),"b", label = "k-3rd")
-plt.xlabel("Number of words")
-plt.ylabel("Adjusted log frequency")
-plt.legend(loc='best')
-plt.show()
-
-
-
-df = df.head(10000)
-
-plt.figure()
-plt.title("Top " + "10,000" + " words")
-plt.plot(np.log(df["k1_freq_adj"]),"r", label = "k-1st")
-plt.plot(np.log(df["k3_freq_adj"]),"b", label = "k-3rd")
-plt.xlabel("Number of words")
-plt.ylabel("Ajusted log frequency")
-plt.legend(loc='best')
-plt.show()
-
-
-
-df = df.head(1000)
-
-plt.figure()
-plt.title("Top " + "1,000" + " words")
-plt.plot(np.log(df["k1_freq_adj"]),"r", label = "k-1st")
-plt.plot(np.log(df["k3_freq_adj"]),"b", label = "k-3rd")
-plt.xlabel("Number of words")
-plt.ylabel("Adjusted log frequency")
-plt.legend(loc='best')
-plt.show()
+df = adjusted_df
+for num_rows in num_rows_values:
+        df = df.head(num_rows)
+        print("k-1st words: ",df["is_k1_word"].sum() )
+        print("k-3rd words: ",df["is_k3_word"].sum() )
+        plt.figure()
+        plt.title("Top " + "{:,}".format(num_rows) + " words")
+        plt.plot(np.log(df["k1_freq_adj"]),"r", label = "k-1st")
+        plt.plot(np.log(df["k3_freq_adj"]),"b", label = "k-3rd")
+        plt.xlabel("Number of words")
+        plt.ylabel("Adjusted log frequency")
+        plt.legend(loc='best')
+        plt.show()
